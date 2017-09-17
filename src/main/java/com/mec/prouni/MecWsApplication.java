39efad450da42e7d2f8a041aaca73e7712ee6a7f
Alessandro Rezende <alessandropereirarezende@gmail.com>
2017-09-17 17:19:03 -0300

    Projeto mec-ws: alterado config postgres

--- src/main/java/com/mec/prouni/MecWsApplication.java
+++ src/main/java/com/mec/prouni/MecWsApplication.java
@@ -14,34 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class MecWsApplication {
-	
-
-	@Autowired
-	private DataSource dataSource;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MecWsApplication.class, args);
 	}
 	
-	@RequestMapping("/db")
-	String db(Map<String, Object> model) {
-		try (Connection connection = dataSource.getConnection()) {
-			Statement stmt = connection.createStatement();
-			stmt.executeUpdate("CREATE TABLE IF NOT EXISTS convidados (id bigint NOT NULL,nome character varying(255), quantidadeAcompanhantes integer)");
-			/*stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-			ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-
-			ArrayList<String> output = new ArrayList<String>();
-			while (rs.next()) {
-				output.add("Read from DB: " + rs.getTimestamp("tick"));
-			}
-
-			model.put("records", output);*/
-			return "db";
-		} catch (Exception e) {
-			model.put("message", e.getMessage());
-			return "error";
-		}
-	}
-
 }
