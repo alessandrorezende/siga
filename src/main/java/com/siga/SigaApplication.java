0c34332576afcbddb0b3b1e8a9d8f12baf5e2b5e
Alessandro Rezende <alessandropereirarezende@gmail.com>
2017-09-19 01:58:47 -0300

    Commit alterações no front-end

--- src/main/java/com/siga/SigaApplication.java
+++ src/main/java/com/siga/SigaApplication.java
@@ -3,7 +3,11 @@ package com.siga;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.siga.model.Usuario;
 
 @Controller
 @SpringBootApplication
@@ -13,8 +17,19 @@ public class SigaApplication {
 		SpringApplication.run(SigaApplication.class, args);
 	}
 	
+	
 	@RequestMapping("/")
-	String index() {
-		return "login";
+	public ModelAndView login() {
+		ModelAndView modelAndView = new ModelAndView("login");
+		modelAndView.addObject(new Usuario());
+		return modelAndView;
+	}
+	
+
+	@RequestMapping("/index")
+	public ModelAndView index() {
+		ModelAndView modelAndView = new ModelAndView("index");
+		modelAndView.addObject(new Usuario());
+		return modelAndView;
 	}
 }
--- src/main/java/com/siga/controller/LoginController.java
+++ src/main/java/com/siga/controller/LoginController.java
@@ -2,9 +2,12 @@ package com.siga.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.siga.model.Convidado;
 import com.siga.model.Usuario;
 import com.siga.repository.Usuarios;
 
@@ -13,7 +16,6 @@ import com.siga.repository.Usuarios;
 public class LoginController {
 	@Autowired
 	private Usuarios usuarios;
-
 	
 	@PostMapping
 	public String tryLogin(Usuario usuario) {
